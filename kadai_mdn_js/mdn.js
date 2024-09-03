99f1a5d07996eba0aedd80f68a72b76ac2c5b96b
kou181 <regard_fm@yahoo.co.jp>
2024-09-03 14:50:41 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+let today;
+today = new Date();
+console.log(today);
+let year = today.getFullYear();
+let month = today.getMonth() + 1;
+let day = today.getDate();
+console.log(year+'年'+month+'月'+day+'日');
