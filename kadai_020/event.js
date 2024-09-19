7d01a860b2753745f520614841d8cf233bea8cc1
kou181 <regard_fm@yahoo.co.jp>
2024-09-19 15:36:25 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const firstBtn = document.getElementById('btn');
+
+const hyouji = document.getElementById('text');
+
+firstBtn.addEventListener('click',()=>{
+    hyouji.textContent = 'ボタンをクリックしました';
+})
